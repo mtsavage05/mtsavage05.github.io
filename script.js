7e87afbd9808838eb511987d6705319193c485ff
mtsavage05 <mtsavage05@gmail.com>
2025-09-18 11:22:10 -0500

    thing

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-/*
-function updateName() {
-  const name = prompt("Enter a new name");
 
-  button.textContent = `${name}`;
-  return name
-}
-
-function createArtical() {
-    const art = document.createElement('article');
-    art.textContent = 'fred'
-    document.body.appendChild(art);
-}
-*/
 function createParagraph() {
   const para = document.createElement("button");
   para.textContent = "You clicked the button!";
