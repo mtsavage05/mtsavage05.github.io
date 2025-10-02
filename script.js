42acf649a63f97580062831f7e0488720aba54a2
mtsavage05 <mtsavage05@gmail.com>
2025-10-02 15:15:45 -0500

    might be working?

--- script.js
+++ script.js
@@ -1,87 +1,130 @@
 
-function addListItemName(text) {
-    var li = document.createElement("li");
-    li.textContent = text;
+// function addListItemName(text) {
+//     var li = document.createElement("li");
+//     li.textContent = text;
 
-    var deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.onclick = function() {
-        li.remove();
+//     var deleteBtn = document.createElement("button");
+//     deleteBtn.textContent = "Delete";
+//     deleteBtn.onclick = function() {
+//         li.remove();
+//     };
+//     var changeBtn = document.createElement("button");
+//     changeBtn.textContent = "Edit"
+//     changeBtn.onclick = function() {
+//         li.toggleAttribute('contenteditable');
+//     }
+    
+//     li.appendChild(changeBtn);
+
+//     li.appendChild(deleteBtn);
+//     document.getElementById("itemListName").appendChild(li);
+// }
+
+//  function addListItemDate(text) {
+//      var li = document.createElement("li");
+//      li.textContent = text;
+
+//      var deleteBtn = document.createElement("button");
+//      deleteBtn.textContent = "Delete";
+    
+//      deleteBtn.onclick = function() {
+//          li.remove();
+//      };
+//      var changeBtn = document.createElement("button");
+//      changeBtn.onclick = function() {
+//          li.toggleAttribute('contenteditable');
+//      }
+    
+//      li.appendChild(changeBtn);
+
+//      li.appendChild(deleteBtn);
+//      document.getElementById("itemListDate").appendChild(li);
+//  }
+
+//  function addListItemStatus(text) {
+//      var li = document.createElement("li");
+//      li.textContent = text;
+
+//      var deleteBtn = document.createElement("button");
+//      deleteBtn.textContent = "Delete";
+//      deleteBtn.onclick = function() {
+//          li.remove();
+//      }
+//      var changeBtn = document.createElement("button");
+//      changeBtn.textContent = "Edit"
+//      changeBtn.onclick = function() {
+//          li.toggleAttribute('contenteditable');
+//      }
+    
+//      li.appendChild(changeBtn);
+//      li.appendChild(deleteBtn);
+    
+//      document.getElementById("itemListStatus").appendChild(li);
+//  }
+
+
+// document.getElementById("addBtnName").onclick = function() {
+//     var text = document.getElementById("userInputName").value;
+//     if (text.trim() !== "") {
+//         addListItemName(text);
+//         document.getElementById("userInputName").value = "";
+//     }
+// }
+// document.getElementById("addBtnDate").onclick = function() {
+//     var text = document.getElementById("userInputDate").value;
+//     if (text.trim() !== "") {
+//         addListItemDate(text);
+//         document.getElementById("userInputDate").value = "";
+//     }
+// }
+
+// document.getElementById("addBtnStatus").onclick = function() {
+//     var text = document.getElementById("userInputStatus").value;
+//     if (text.trim() !== "") {
+//         addListItemStatus(text);
+//         document.getElementById("userInputStatus").value = "";
+//     }
+// }
+
+const searchButton = document.getElementById("searchButton");
+const apiResultsContainer = document.getElementById("apiResultsContainer");
+
+
+function renderResults(todos) {
+    apiResultsContainer.innerHTML = "";
+    if (todos.length === 0) {
+        apiResultsContainer.textContent = "No results"
+    }
+    todos.array.forEach(item => {
+        const div = document.createElement("div");
+        div.textContent = item.todo;
+        apiResultsContainer.appendChild(div);
+    });
+}
+
+
+searchButton.addEventListener("click", () => {
+    const searchInput = document.getElementById("searchInput");
+    const query = searchInput.value.trim();
+    if (!query) {
+        return;
     };
-    var changeBtn = document.createElement("button");
-    changeBtn.textContent = "Edit"
-    changeBtn.onclick = function() {
-        li.toggleAttribute('contenteditable');
-    }
+    apiResultsContainer.textContent = "Loading";
+    fetch(`https://dummyjson.com/todos/search?q=${encodeURIComponent(query)}&limit=10`)
+    .then(Response => Response.json())
+    .then(data => {
+        renderResults(data.todos)
+    })
+    .catch(error => {
+        // catch errors
+    });
+
+});
+
+
+
+
+
+
+
     
-    li.appendChild(changeBtn);
-
-    li.appendChild(deleteBtn);
-    document.getElementById("itemListName").appendChild(li);
-}
-function addListItemDate(text) {
-    var li = document.createElement("li");
-    li.textContent = text;
-
-    var deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    
-    deleteBtn.onclick = function() {
-        li.remove();
-    };
-    var changeBtn = document.createElement("button");
-    changeBtn.textContent = "Edit"
-    changeBtn.onclick = function() {
-        li.toggleAttribute('contenteditable');
-    }
-    
-    li.appendChild(changeBtn);
-
-    li.appendChild(deleteBtn);
-    document.getElementById("itemListDate").appendChild(li);
-}
-
-function addListItemStatus(text) {
-    var li = document.createElement("li");
-    li.textContent = text;
-
-    var deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.onclick = function() {
-        li.remove();
-    }
-    var changeBtn = document.createElement("button");
-    changeBtn.textContent = "Edit"
-    changeBtn.onclick = function() {
-        li.toggleAttribute('contenteditable');
-    }
-    
-    li.appendChild(changeBtn);
-    li.appendChild(deleteBtn);
-    
-    document.getElementById("itemListStatus").appendChild(li);
-}
-
-
-document.getElementById("addBtnName").onclick = function() {
-    var text = document.getElementById("userInputName").value;
-    if (text.trim() !== "") {
-        addListItemName(text);
-        document.getElementById("userInputName").value = "";
-    }
-}
-document.getElementById("addBtnDate").onclick = function() {
-    var text = document.getElementById("userInputDate").value;
-    if (text.trim() !== "") {
-        addListItemDate(text);
-        document.getElementById("userInputDate").value = "";
-    }
-}
-
-document.getElementById("addBtnStatus").onclick = function() {
-    var text = document.getElementById("userInputStatus").value;
-    if (text.trim() !== "") {
-        addListItemStatus(text);
-        document.getElementById("userInputStatus").value = "";
-    }
-}
